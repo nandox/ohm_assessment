20fef63f18b17a0117e79c11a6385395d7425be7
Fernando Sanchez <fernando.sanchez@avantica.net>
2020-02-08 22:21:15 -0600

    - Added unversioned files and folders to gitignore. - Added new test. - Modified user model and header bar.

--- tests/models_tests/user_test.py
+++ tests/models_tests/user_test.py
@@ -5,3 +5,7 @@ class UserTest(OhmTestCase):
     def test_get_multi(self):
         assert self.chuck.get_multi("PHONE") == ['+14086441234', '+14086445678']
         assert self.justin.get_multi("PHONE") == []
+
+
+    def test_get_points_and_email(self):
+        assert self.chuck.get_points_and_email() == "Points: 0 e-mail: test@test.com"
